f229328a395d1a811ba3d6e52e647e3c6e395c53
david <dbcantidio@gmail.com>
2020-09-06 15:09:04 -0300

    film name search

--- tv.py
+++ tv.py
@@ -2,12 +2,13 @@ from resources import keys, config
 import samsungctl
 import time
 
-char_list = [["a","b","c","d","e","f"],["g","h","i","j","k","l"],["m","n","o","p","q","r"],["s","t","u","v","w","x"],["y","z","1","2","3","4"],["5","6","7","8","9","0"]]
+char_list = [["a", "b", "c", "d", "e", "f"], ["g", "h", "i", "j", "k", "l"], ["m", "n", "o", "p", "q", "r"],
+             ["s", "t", "u", "v", "w", "x"], ["y", "z", "1", "2", "3", "4"], ["5", "6", "7", "8", "9", "0"]]
 film_name = "friends"
 
 
 def get_char_map(film_name, char_list):
-    char_map =[]
+    char_map = []
     for letter in film_name:
         for row in char_list:
             for item in row:
@@ -15,44 +16,62 @@ def get_char_map(film_name, char_list):
                     n_row = char_list.index(row)
                     n_col = row.index(letter)
                     char_map.append((n_row, n_col))
-                    #print(f"letra {letter}, coordenadas row {n_row} e col {n_col}")
+                    print(f"letra {letter}, coordenadas row {n_row} e col {n_col}")
     return char_map
-
 
 
 def move(*args):
     with samsungctl.Remote(config) as remote:
         c_row = 0
         c_col = 0
+        print(f"começando na linha {c_row}, e na coluna  {c_col} ")
         for tuples in args:
             for item in tuples:
                 t_row = item[0]
                 t_col = item[1]
-                coordinate_row = c_row-t_row
-		coordinate_col = c_col-t_col
+                print(f"e tenho que ir para a linha  {t_row}, e  coluna  {t_col} ")
+                coordinate_row = c_row - t_row
+                coordinate_col = c_col - t_col
+                print(f" para isso tenho que andar  {coordinate_row} linhas e {coordinate_col}  colunas")
                 i = 0
-                while i <= abs(coordinate_row):
-		    if coordinate_row < 0:
-		    remote.control((keys['right key']))
-	            time.sleep(0.5)
-		    i += 1
-		    elif coordinate_row > 0:
-		    remote.control((keys['left key']))
-		    time.sleep(0.5)
-		    i += 1
-	        while i <= abs(coordinate_row):
-		    if coordinate_row < 0:
-		    remote.control((keys['right key']))
-		    time.sleep(0.5)
-		    i += 1
-		    elif coordinate_row > 0:
-		    remote.control((keys['left key']))
-		    time.sleep(0.5)
-		    i += 1
-		    remote.control((keys['enter key']))
+                print(f" descobrindo se vou para cima, para baixo ou se fico...")
+                while i < abs(coordinate_row):
+                    if coordinate_row < 0:
+                        print(f" andando uma casa  para baixo...")
+                        remote.control((keys['down key']))
+                        time.sleep(0.5)
+                        i += 1
+                    elif coordinate_row > 0:
+                        remote.control((keys['up key']))
+                        print(f" andando uma casa para cima...")
+                        time.sleep(0.5)
+                        i += 1
+                    else:
+                        print("ja estou na linha certa vamos para a coluna certa agora...")
+                        i += 1
+                i = 0
+                print(f" descobrindo se vou para direita ou para esquerda...")
+                while i < abs(coordinate_col):
+                    if coordinate_col < 0:
+                        print(f" andando uma casa para direita...")
+                        remote.control((keys['right key']))
+                        time.sleep(0.5)
+                        i += 1
+                    elif coordinate_col > 0:
+                        print(f" andando uma casa para esquerda...")
+                        remote.control((keys['left key']))
+                        time.sleep(0.5)
+                        i += 1
+                print ("não preciso mais mudar de coluna, apertando enter...")
+                c_row = t_row
+                c_col = t_col
+                print(f"agora estou na linha {c_row} e coluna {c_col} ")
+                i += 1
+                remote.control((keys['enter key']))
 
 
-def netflix_on ():
+
+def netflix_on():
     with samsungctl.Remote(config) as remote:
         remote.control((keys['return key']))
         time.sleep(2)
@@ -74,7 +93,7 @@ def netflix_off():
         remote.control((keys['return key']))
 
 
-def forward_medium_on ():
+def forward_medium_on():
     with samsungctl.Remote(config) as remote:
         remote.control((keys['enter key']))
         time.sleep(0.5)
@@ -128,20 +147,20 @@ def rewind_little_on():
             time.sleep(0.5)
 
 
-#for play, pause and enter
+# for play, pause and enter
 def play():
     with samsungctl.Remote(config) as remote:
         remote.control((keys['enter key']))
 
 
-def play_from_beginning ():
+def play_from_beginning():
     with samsungctl.Remote(config) as remote:
         remote.control((keys['down key']))
         time.sleep(1)
         remote.control((keys['enter key']))
 
 
-def get_out ():
+def get_out():
     with samsungctl.Remote(config) as remote:
         remote.control((keys['return key']))
 
@@ -161,7 +180,7 @@ def zap_left():
             remote.control((keys['left key']))
 
 
-def turn_off ():
+def turn_off():
     with samsungctl.Remote(config) as remote:
         remote.control((keys['poweroff key']))
 
